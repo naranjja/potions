5bda59b30bcb0a8b16e258e19f06fd1b8cb06f3e
Jose Naranjo <naranjja@gmail.com>
2022-08-18 11:24:17 -0500

    :sparkles: Add loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,26 @@ import { ThemeProvider } from "@mui/material";
 import { theme } from "../utils/theme";
 import createEmotionCache from "../utils/createEmotionCache";
 import { CacheProvider } from "@emotion/react";
+import NextNProgress from 'nextjs-progressbar';
 
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({
- Component,
- emotionCache = clientSideEmotionCache,
- pageProps,
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
 }) {
- return (
-   <CacheProvider value={emotionCache}>
-     <ThemeProvider theme={theme}>
-       <Component {...pageProps} />
-     </ThemeProvider>
-   </CacheProvider>
- );
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <NextNProgress
+          color="#fff"
+          height={5}
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
